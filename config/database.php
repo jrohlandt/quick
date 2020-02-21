1acb38750a2f2c53f6fe09e9615ad0239d7d0599
Jeandré Rohlandt <jrohlandt@gmail.com>
2020-02-21 21:39:10 +0200

    Add mongo db port variable.

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'host' => env('MONGO_DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
+            'port' => env('MONGO_DB_PORT', 27017),
             'database' => env('MONGO_DB_DATABASE'),
             'username' => env('MONGO_DB_USERNAME'),
             'password' => env('MONGO_DB_PASSWORD'),
